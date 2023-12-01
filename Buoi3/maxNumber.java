0363f3a616e0c64532b5db45f41576e29d99d899
Thắng Bùi <thangbui@Thangs-MacBook-Pro.local>
2023-12-01 11:04:26 +0700

    first commit

---
+++ Buoi3/maxNumber.java
@@ -0,0 +1,20 @@
+package Buoi3;
+import java.util.Scanner;
+
+public class maxNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        int b;
+        System.out.println("Nhap a: ");
+        a = sc.nextInt();
+        System.out.println("Nhap b: ");
+        b = sc.nextInt();
+        if (a > b) {
+            System.out.printf("Max number is : %d\n",a);
+        } else {
+            System.out.printf("Max number is : %d\n",b);
+        }
+        sc.close();
+    }
+}
