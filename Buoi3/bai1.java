70bc8ace251ec50adcfbecf65d7bc7c6458a7624
Thắng Bùi <thangbui@Thangs-MacBook-Pro.local>
2023-12-01 13:22:07 +0700

    ok

--- Buoi3/bai1.java
+++ Buoi3/bai1.java
@@ -1,7 +1,7 @@
 package Buoi3;
 import java.util.Scanner;
 
-public class maxNumber {
+public class bai1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a;
